6700571f285649f544c679e9cb595e22162bf802
Noah Alayon <noahalayon01@gmail.com>
2025-06-21 21:49:04 -0400

    Added constants.h file

---
+++ src/constants.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <iostream>
+using namespace std;
+
+const int ROWS = 4, COLS = 4;
+const string null_cell = "";
+const string empty_cell = ".";
+const string block_sprite = "█";
+const string item_sprite = "♠";
+
+const string player_sprite = "@";
+const string enemy_sprite = "%";
