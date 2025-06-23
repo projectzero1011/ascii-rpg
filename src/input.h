72ad9ae8db111b9769d9a8470b4ec9e97f2d7ee1
Noah Alayon <noahalayon01@gmail.com>
2025-06-22 21:43:56 -0400

    Added input .h and .cpp files

---
+++ src/input.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <Key.h>
+#include <Selection.h>
+#include <Option.h>
+#include <iostream>
+using namespace std;
+
+Selection title_input(string message);
+Key world_input(string message);
+Option battle_input(string message);
