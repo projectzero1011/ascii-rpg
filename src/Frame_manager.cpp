ac710cd22ee8349f6e83b27f8eb85f6765c47690
Noah Alayon <noahalayon01@gmail.com>
2025-06-24 21:55:03 -0400

    Added play_cutscene to Frame_manager files

--- src/Frame_manager.cpp
+++ src/Frame_manager.cpp
@@ -3,6 +3,18 @@
 #include <prompt.h>
 #include <fstream>
 
+Frame_manager::Frame_manager() {
+    for(string fname : frame_tbl) {
+        string f = load(fname);
+        frames.push_back(f);
+    }
+
+    for(string bfname : battle_frame_tbl) {
+        string bf = load(bfname);
+        battle_frames.push_back(bf);
+    }
+}
+
 string load(string& name) {
     ifstream ist {"../files/" + name + ".txt"};
     if (!ist) throw runtime_error("Can't open input file");
@@ -25,14 +37,47 @@ void show_help_battle(const Frame_manager& fm) {
     prompt_next("Back",Frame::help_battle,fm);
 }
 
-Frame_manager::Frame_manager() {
-    for(string fname : frame_tbl) {
-        string f = load(fname);
-        frames.push_back(f);
+void play_cutscene(const Frame_manager& fm, Frame f, string in_file) {
+    ifstream ist {in_file};
+    if (!ist) throw runtime_error("Can't open file!");
+
+    // Top border
+    clear_screen();
+    fm.print(f);
+    cout << "════════════════════════\n ";
+
+    int total_char_count = 0;
+    int line_char_count = 0;
+
+    for(string word; ist >> word;) {
+        total_char_count += word.size()+1;
+        line_char_count += word.size()+1;
+
+        if(total_char_count > 46) {
+            // Bottom border
+            cout << "\n════════════════════════\n\n";
+            prompt_next("Next");
+
+            // Top border
+            clear_screen();
+            fm.print(f);
+            cout << "════════════════════════\n ";
+
+            // Reset char counts
+            total_char_count = word.size()+1;
+            line_char_count = word.size()+1;
+        }
+
+        if(line_char_count < 23) {
+            cout << word << " ";
+        }
+        else {
+            cout << "\n " << word << " ";
+            line_char_count = word.size()+1;
+            total_char_count += word.size()+1;
+        }
     }
 
-    for(string bfname : battle_frame_tbl) {
-        string bf = load(bfname);
-        battle_frames.push_back(bf);
-    }
+    if (total_char_count < 25) cout << "\n";
+    cout << "\n════════════════════════\n\n";
 }
--- src/Frame_manager.h
+++ src/Frame_manager.h
@@ -7,7 +7,7 @@ public:
     Frame_manager();
     void print(Frame f) const { cout << frames[int(f)]; }
     void print(Battle_frame bf) const { cout << battle_frames[int(bf)]; }
-    // void play_cutscene(Frame f);
+    // void play_cutscene(Frame f, Cutscene c);
 private:
     vector<string> frames;
     vector<string> battle_frames;
@@ -17,9 +17,11 @@ private:
 string load(string& name);
 void show_help_world(const Frame_manager& fm);
 void show_help_battle(const Frame_manager& fm);
+void play_cutscene(const Frame_manager& fm, Frame f, string in_file);
 
 // vector<vector<string>> will contain dialogue for each cutscene
 // outer vector contains entire script of the game
 // inner vector contains script of particular cutscenes
 // have separate files for each cutscene script
 // read into vector script line by line
+// enum class Cutscene, with vector<string> cutscene_tbl
