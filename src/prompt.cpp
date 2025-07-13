c39d6d8b2d3880bde77c998436e429330792bd21
Noah Alayon <noahalayon01@gmail.com>
2025-07-13 16:52:22 -0400

    Updated prompt_next functions

--- src/prompt.cpp
+++ src/prompt.cpp
@@ -4,31 +4,32 @@
 #include <Battle.h>
 #include <utils.h>
 
-void prompt_next(string message) {
-    char ch = get_char(message + " [z: next]");
+void prompt_next(string msg) {
+    char ch = get_char(msg + " [z: next]");
 }
 
-void prompt_next(string message, const World_map& world) {
+void prompt_next(string msg, const World_map& world) {
     char ch = ' ';
     while(ch != 'z') {
-        ch = get_char(message + " [z: next]");
+        ch = get_char(msg + " [z: next]");
         world.refresh();
     }
 }
 
-void prompt_next(string message, Battle& battle) {
+void prompt_next(string msg, Battle& battle) {
     battle.refresh_last();
     char ch = ' ';
     while(ch != 'z') {
-        ch = get_char(message + " [z: next]");
+        ch = get_char(msg + " [z: next]");
         battle.refresh_last();
     }
 }
 
-void prompt_next(string message, Frame f, const Frame_manager& fm) {
+// Only used for show_help_world() and show_help_battle()
+void prompt_next(string msg, Frame f, const Frame_manager& fm) {
     char ch = ' ';
-    while(ch != 'z') {
-        ch = get_char(message + " [z: next]");
+    while(ch != 'x') {
+        ch = get_char(msg + " [x: back]");
         clear_screen();
         fm.print(f);
     }
